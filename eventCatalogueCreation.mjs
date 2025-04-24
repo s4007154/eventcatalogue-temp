d85385c1f42e4d7a9bb25c6ad88d139032dfd2ba
s4007154 <s4007154@student.rmit.edu.au>
2025-04-24 16:34:32 +1000

    initalisation

---
+++ eventCatalogueCreation.mjs
@@ -0,0 +1,43 @@
+import utils from '@eventcatalog/sdk';
+
+const { writeDomain, writeServiceToDomain, writeEventToService, addServiceToDomain, addEventToService } = utils('temp-catalog');
+
+
+await writeDomain({
+    id: 'Payment',
+    name: 'Payment domain',
+    version: '0.0.1',
+    summary: 'Domain for all things to do with payments',
+    markdown: '# Hello world',
+});
+
+
+await writeServiceToDomain({
+    id: 'InventoryService',
+    name: 'Inventory Service',
+    version: '0.0.1',
+    summary: 'Service that handles the inventory',
+    markdown: '# Hello world',
+}, { id: 'Payment' });
+
+await addServiceToDomain('Payment', { id: 'InventoryService', version: '0.0.1' });
+
+// await writeEventToService({
+//     id: 'InventoryAdjusted',
+//     name: 'Inventory Adjusted',
+//     version: '0.0.1',
+//     summary: 'This is a summary',
+//     markdown: '# Hello world',
+//     }, { id: 'InventoryService' });
+
+// await writeEventToService({
+//     id: 'InventoryCleared',
+//     name: 'Inventory Cleared',
+//     version: '0.0.1',
+//     summary: 'This is a summary',
+//     markdown: '# Hello world',
+//     }, { id: 'InventoryService' });
+
+// await addEventToService('InventoryService', 'receives', { id: 'InventoryAdjusted', version: '0.0.1' });
+
+// await addEventToService('InventoryService', 'receives', { id: 'InventoryCleared', version: '0.0.1' });
---
+++ temp-catalog/eventcatalog.config.js
@@ -0,0 +1,34 @@
+/** @type {import('@eventcatalog/core/bin/eventcatalog.config').Config} */
+export default {
+  title: 'EventCatalog',
+  tagline: 'This internal platform provides a comprehensive view of our event-driven architecture across all systems. Use this portal to discover existing domains, explore services and their dependencies, and understand the message contracts that connect our infrastructure',
+  organizationName: 'Temp Catalogue',
+  homepageLink: 'https://eventcatalog.dev/',
+  editUrl: 'https://github.com/boyney123/eventcatalog-demo/edit/master',
+  // By default set to false, add true to get urls ending in /
+  trailingSlash: false,
+  // Change to make the base url of the site different, by default https://{website}.com/docs,
+  // changing to /company would be https://{website}.com/company/docs,
+  base: '/',
+  // Customize the logo, add your logo to public/ folder
+  logo: {
+    alt: 'EventCatalog Logo',
+    src: '/logo.png',
+    text: 'EventCatalog'
+  },
+  docs: {
+    sidebar: {
+      // TREE_VIEW will render the DOCS as a tree view and map your file system folder structure
+      // LIST_VIEW will render the DOCS that look familiar to API documentation websites
+      type: 'LIST_VIEW'
+    },
+  },
+  // Enable RSS feed for your eventcatalog
+  rss: {
+    enabled: true,
+    // number of items to include in the feed per resource (event, service, etc)
+    limit: 20
+  },
+  // required random generated id used by eventcatalog
+  cId: '43940de0-2702-4bc7-b332-a394343c7b14'
+}
